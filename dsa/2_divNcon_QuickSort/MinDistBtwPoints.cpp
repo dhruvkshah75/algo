91b6fe1fb4abcc2fdfbe2e2a69a5c5acf722ee41
Dhruv Shah <dhruvkshah75@gmail.com>
2026-01-27 21:32:53 +0530

    feat: Solved 3 easy questions from CSF211 Lab2 Sheet

---
+++ dsa/2_divNcon_QuickSort/MinDistBtwPoints.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// question link:
+
+// 3rd HARD question 
+
+class Solution {
+public:
+    double minDist(vector<vector<int>>&points) {
+
+    } 
+};
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<vector<int>> points(n, vector<int>(2));
+    for(int i=0; i<n; i++) {
+        cin >> points[i][0] >> points[i][1];
+    }
+
+    Solution s = Solution();
+}
