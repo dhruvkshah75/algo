c68ca74e8b83bbfb8ed7ef57c9f8a44177c84f4b
Dhruv Shah <dhruvkshah75@gmail.com>
2026-02-18 02:51:01 +0530

    fixed the loc of the nge code

--- Lab5-CSF211/nge.cpp
+++ Lab5-CSF211/nge.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 // Implementation code of NEXT GREATER ELEMENT 
 
+// ====== Using Normal STACK ========
 vector<int> nextGreaterElement(vector<int>&nums) {
     // find the next greater element 
     int n = nums.size();
@@ -22,3 +23,47 @@ vector<int> nextGreaterElement(vector<int>&nums) {
 
     return nge;
 }
+
+
+
+// implementation of nect greater element using Monotonic decreasing stack 
+// ========= Using MONOTONIC DECREASING STACK ========
+
+/*
+    In this backward traversal => makes more sense as i want to find the nge on the right for the element => nums[i] 
+    that means we must have traversed all the elements in the backward direction 
+    eg 
+    4 12 5 3 1 2 5 |3| 1 2 4 6 
+    assume we want to get the nge of the marked 3 then what do we do ?
+    we traverse back from 6 then maintain a montonic stack 
+*/
+
+vector<int> ngeBackward(vector<int> &nums) {
+    int n = nums.size();
+    // we traverse in the backward direction 
+    vector<int> result(n, -1);
+    stack<int> st;
+    for(int i=n-1; i>=0; i--) {
+        int curr = nums[i];
+        // if curr >= st.top element then we must remove these elements and store curr at the correct location 
+        while(!st.empty() && curr >= nums[st.top()]) {  
+            st.pop();
+        }
+        // now the st.top is the nge of the current element 
+        if(!st.empty()) {
+            result[i] = nums[st.top()];
+        }
+        st.push(i);
+    }
+
+    return result;
+}
+
+/*
+    NOTE:
+    In a Forward Scan, you look for the "Next Greater" by having elements wait in a stack until a "boss" 
+    (the current element) arrives to resolve them.
+
+    In Backward Scan, you look for the "Next Greater" by looking into the "Future" (the stack) 
+    and picking the best candidate.
+*/
